27f4bd77b9735ebee6a1787fabca5d3a793726c4
Catalin Enache <catalinenache78@gmail.com>
2024-06-03 18:36:57 +0300

    Updates

--- src/lib/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export const degToRad = (deg: number) => (deg / 180) * Math.PI;
-export const radToDeg = (rad: number) => (rad / Math.PI) * 180;
-export const radToDegFormatter = (rad: number) => radToDeg(rad).toFixed(3);
